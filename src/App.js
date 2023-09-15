7c069052212c12eec6b03d4e96ce16bc4734fa3f
laura <l.restrepop@uniandes.edu.co>
2023-09-14 19:07:57 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import titleData from './Titles.json';
+import questionData from './Questions.json';
+import answerData from './Answers.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [title, setTitle] = useState(0);
+    const [question, setQuestion] = useState(0);
+    const [answer, setAnswer] = useState(0);
+
+    const renderTitle = () => { return titleData.find(item => item.id === title).title }; 
+    const renderQuestion = () => { return questionData.find(item => item.id === question).question };
+    const renderAnswer = () => { 
+        if (answer === 4){ return <img class="Image" src={answerData.find(item => item.id === answer).answer} alt="Virtual DOM"/> }
+        else { return answerData.find(item => item.id === answer).answer }
+        };
+
+    const handleRestart = () => {setTitle(0); setQuestion(0); setAnswer(0)};
+    const handlePrev = () => {if (title > 0) {setTitle(title-1); setQuestion(question-1); setAnswer(answer-1)}};
+    const handleNext = () => {if (title < 4) {setTitle(title+1); setQuestion(question+1); setAnswer(answer+1)}};
+
+    return(
+        <div>
+            <div name="Botones" class="Buttons">
+                <button type="button" class="Button" onClick={handleRestart}> Restart </button>
+                <button type="button" class="Button" onClick={handlePrev}> Prev </button>
+                <button type="button" class="Button" onClick={handleNext}> Next </button>
+            </div>   
+            <hr></hr>
+            <div name="Titulo&PRegunta">
+                <div class="Title">{renderTitle()}</div>
+                <div class="Question">{renderQuestion()}</div>
+            </div>
+            <hr></hr>
+            <div name="Respuesta">
+                <div class="Answer">{renderAnswer()}</div>
+            </div>
+        </div>
+        
+    );
 }
 
 export default App;
